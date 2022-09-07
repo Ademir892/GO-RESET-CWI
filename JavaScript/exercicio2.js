d0cbb09202a1f6999cb0d0493853c7b2b27096c3
Ademir892 <ademirtraesel@hotmail.com>
2022-09-07 09:30:53 -0300

    Exercicios 2

---
+++ JavaScript/exercicio2.js
@@ -0,0 +1,29 @@
+nomeDoFulamo = "Jason";
+nomeDoBeltrano = "Ana";
+
+mesmoNome = (nomeDoFulamo == nomeDoBeltrano);
+
+idade = 17;
+maiorIdade = (idade >= 18);
+
+valorBoleto = 100;
+valorComJuros = (valorBoleto * 0.1);
+
+item1 = 5;
+item2 = 3;
+item3 = 8;
+item4 = 10;
+item5 = 15;
+mediaAritmetica = (item1 + item2 + item3 + item4 + item5) /5;
+
+
+lucroBruto = (receitaLiquida - custosDosProdutosVendidos);
+margemBruta = (lucroBruto / receitaLiquida) * 100;
+
+saldo = 1000.00;
+saldo = saldo - 99.90;
+saldo = saldo + 2500.00;
+saldo = saldo - 0.10;
+saldo = saldo * 0.75;
+saldo = saldo / 2;
+console.log(saldo);
